722a7975b449082db9e581f8cfdfcb371fe0ed83
imranlielchin <elchinii@code.edu.az>
2022-12-06 20:02:24 +0400

    init

---
+++ HomeWorkWay2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace HomeWorkWay2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        //{
+        //    Console.WriteLine("First Number:");
+        //    int firstNumber = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("Second Number");
+        //    int secondNumber = Convert.ToInt32((Console.ReadLine()));
+        //    Console.WriteLine("Total:");
+        //    Console.WriteLine(firstNumber + secondNumber);
+        {
+            Console.WriteLine("First Number:");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Second Number:");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Divided:");
+            Console.WriteLine(firstNumber%secondNumber);
+        }
+
+
+     
+
+
+
+
+        }
+    }
